44306437960ed28462596205af3bde9cae588a35
yefersonsistemas <43937509+yefersonsistemas@users.noreply.github.com>
2022-09-13 19:54:28 -0400

    Second commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="mt-24">
+    <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+      © 2022 Diseñado y desarrollado por Yeferson Morales
+    </p>
+  </div>
+);
+
+export default Footer;
